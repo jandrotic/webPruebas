6c2b1f4a584265472400246a50a0e4797da5850d
jandrotic <alvarezjandro@gmail.com>
2015-08-05 13:54:35 +0200

    Primer commit

---
+++ programacionjs.js
@@ -0,0 +1,18 @@
+alert("Hola Mundo");
+console.log("Mensaje que sale en la consola");
+
+var apellido = "Alvarez";
+console.log(apellido);
+
+var edad = 30;
+
+if(edad >= 18){
+	console.log("Es mayor de edad");
+}else{
+	console.log("Es menor de edad");
+}
+
+for(var i=0; i < 10; i++){
+  console.log("Vuelta: "+i);
+}
+console.log("Terminó el for");
